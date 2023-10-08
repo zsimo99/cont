148d406a18b1d6f3f209c65cb30ad61d99051782
zsimo <mohamedzoraa100@gmail.com>
2023-10-08 19:12:33 +0100

    initialCommit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Hero from "./Components/Hero"
+
+const App = () => {
+    return (
+        <div className="overflow-hidden">
+            <Hero />
+            <div className="h-[5000px]"></div>
+        </div>
+    )
+}
+
+export default App
---
+++ src/Components/Hero.jsx
@@ -0,0 +1,12 @@
+
+const Hero = () => {
+    return (
+        <div className='h-screen w-screen hero '>
+            <div className="headerMask">
+                <h1 className='font-header text-[#29232b] fixed top-20 left-1/2 -translate-x-1/2 text-center text-2xl sm:text-3xl lg:text-4xl xl:text-6xl font-bold w-full px-10'>Hello!<br /> Everything here is proudly crafted 100% by Me.</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
